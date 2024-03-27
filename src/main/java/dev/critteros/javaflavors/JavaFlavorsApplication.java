57ded3efccac675862a7a69643a8715b7f90e6df
Szymon Kozioł <contact@critteros.dev>
2024-03-27 21:26:48 +0100

    Project bootstrap

---
+++ src/main/java/dev/critteros/javaflavors/JavaFlavorsApplication.java
@@ -0,0 +1,13 @@
+package dev.critteros.javaflavors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFlavorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFlavorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/critteros/javaflavors/JavaFlavorsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.critteros.javaflavors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFlavorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
