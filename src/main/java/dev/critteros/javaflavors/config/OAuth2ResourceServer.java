359ecd5b66766053f3ee9ee08b691217a7fff9e3
Szymon Kozioł <contact@critteros.dev>
2024-05-03 00:44:19 +0200

    Spring Resourse Server configuration

---
+++ src/main/java/dev/critteros/javaflavors/config/OAuth2ResourceServer.java
@@ -0,0 +1,28 @@
+package dev.critteros.javaflavors.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2ResourceServer
+{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/graphiql", "/graphql").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
